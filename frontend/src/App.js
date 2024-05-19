4f8ff0d4d994192b889d8801fcec862d96be148a
renatoxd152 <renatomorillo@gmail.com>
2024-05-19 04:20:54 -0300

    feat: Tela de login de cadastro prontas. Inicio da tela de compras

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import {Cadastrar} from './componentes/Usuario/Cadastrar.js';
+import { Cadastrar } from './componentes/Usuario/Cadastrar.js';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Compras } from './componentes/Compras/Compras.js';
 function App() {
   return (
   
     <BrowserRouter>
       <Routes>
         <Route path='/cadastrar' element={<Cadastrar/>}/>
+        <Route path='/compras' element={<Compras/>}/>
       </Routes>
     </BrowserRouter>
   
--- frontend/src/componentes/Usuario/Cadastrar.js
+++ frontend/src/componentes/Usuario/Cadastrar.js
@@ -1,14 +1,14 @@
 import { Button, Flex, Input, Radio, RadioGroup, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 export const Cadastrar = () =>
 {
     const[cpf,setCPF] = useState("");
     const[senha,setSenha] = useState("");
     const[userType,setUserType] = useState("comum");
-    const handleUserTypeChange = (event)=>
-    {
-        setUserType(event.target.value);
-    }
+    const[isloged,setLogin] = useState(false);
+    const navigate = useNavigate();
+   
     const handleCPF = (event) =>
     {
         setCPF(event.target.value);
@@ -18,22 +18,33 @@ export const Cadastrar = () =>
     const handleSenha = (event) =>{
         setSenha(event.target.value);
     }
+    const handleLogin = () => {
+        setLogin(prevState => !prevState);
+    }
 
     const handleCadastrar = async () =>
     {
         
     }
-    
+
+    const handleLogar = () =>
+    {
+        if(isloged)
+        {
+            navigate("/compras")
+        }
+    }
     return(
         <Flex w="100%" minH="100vh" p="0" m="0">
             <Flex w="30%" minH="100vh" bg="blue" direction={"column"} align={"center"} justify={"center"}>
-                <Text fontSize='3xl' color="white">Acesse sua conta agora mesmo</Text>
-                <Button>Entrar</Button>
+                <Text fontSize='3xl' color="white">{isloged ? 'Cadastre-se agora mesmo!' : 'Acesse sua conta agora mesmo!'}
+                </Text>
+                 <Button onClick={handleLogin}>
+                    {isloged ? 'Cadastrar' : 'Entrar'}
+                </Button>
             </Flex>
             <Flex w="70%" minH="100vh" bg="white" align="center" justify="center" direction="column">
-               
-                    
-                    <Text fontSize='4xl' color="blue">Faça seu cadastro</Text>
+                    <Text fontSize='4xl' color="blue">{isloged? "Faça seu login": "Faça seu cadastro"}</Text>
                     <Flex>
                         <Text direction="column">Digite seu CPF</Text>
                         <Input type='number' value={cpf} onChange={handleCPF}></Input>
@@ -42,14 +53,19 @@ export const Cadastrar = () =>
                         <Text>Digite a senha</Text>
                         <Input type='password' value={senha} onChange={handleSenha}></Input>
                     </Flex>
+
+                    {
+                        !isloged && 
                     <Flex direction="column">
                         <Text>Escolha o tipo de usuário</Text>
                         <RadioGroup value={userType} onChange={setUserType}>
-                            <Radio value='comum'>Comum</Radio>
-                            <Radio value='admin'>Admin</Radio>
+                            <Radio value='COMUM'>Comum</Radio>
+                            <Radio value='ADMIN'>Admin</Radio>
                         </RadioGroup>
                     </Flex>
-                    <Button colorScheme='blue'>Cadastrar</Button>
+                    }
+                    
+                    <Button colorScheme='blue' onClick={handleLogar}>{isloged ? "Login" : "Cadastrar"}</Button>
                 
             </Flex>
         </Flex>
