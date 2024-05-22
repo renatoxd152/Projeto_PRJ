feda047ee7a389c980a24011377facc64a3967b2
renatoxd152 <renatomorillo@gmail.com>
2024-05-21 23:25:40 -0300

    feat: Adicionado listagem de compras e início da tela de cadastro de compras

--- frontend/src/componentes/Compras/CadastrarCompras.js
+++ frontend/src/componentes/Compras/CadastrarCompras.js
@@ -1,11 +1,44 @@
+import { Button, Flex, Select, Text } from "@chakra-ui/react";
 import React from "react";
 import { Nav } from "../../utils/BarraNavegação/Nav";
 
 export const CadastrarCompra = () =>
 {
+    const handleCadastrarCompra = () =>
+    {
+
+    }
     return(
         <>
         <Nav/>
+            <Flex minH="100vh" width="100%" align="center" justify="center">
+                <Flex direction="column">
+                    <Text color="black" fontSize="2xl">Cadastro de compras</Text>
+                    <Flex>
+                        <Text>Selecione um vendedor</Text>
+                        <Select>
+                            <option></option>
+                            <option value='1'>João</option>
+                            <option value='2'>Augusto</option>
+                            <option value='3'>Pedro</option>
+                        </Select>
+                    </Flex>
+
+                    <Flex>
+                        <Text>Selecione um cliente</Text>
+                        <Select>
+                            <option></option>
+                            <option value='1'>Letícia</option>
+                            <option value='2'>Nicole</option>
+                            <option value='3'>Sandra</option>
+                        </Select>
+                    </Flex>
+                    
+
+                    <Button colorScheme="blue" onClick={handleCadastrarCompra} m="4">Cadastrar Compra</Button>
+                </Flex>
+                
+            </Flex>
         </>
     )
 }
--- frontend/src/componentes/Compras/Compras.js
+++ frontend/src/componentes/Compras/Compras.js
@@ -1,11 +1,134 @@
-import React from "react";
+import { Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableCaption, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import React, { useState } from "react";
 import { Nav } from "../../utils/BarraNavegação/Nav";
 
-export const Compras = () =>
-{
-    return(
+export const Compras = () => {
+    const [selectedCompra, setSelectedCompra] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = (compra) => {
+        setSelectedCompra(compra);
+        setIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setSelectedCompra(null);
+        setIsOpen(false);
+    };
+
+    const compras = [
+        { vendedor: "João", cliente: "Sandra", valor: 100.50, data: "21/05/2024" },
+        { vendedor: "João", cliente: "Pedro", valor: 124.50, data: "21/05/2024" }
+    ];
+
+    const produtos = [
+        { nome: "Caneta", preco: 2.50 },
+        { nome: "Lápis", preco: 1.00 },
+        { nome: "Borracha", preco: 0.50 },
+        { nome: "Caderno", preco: 10.00 },
+        { nome: "Régua", preco: 1.50 },
+        { nome: "Tesoura", preco: 3.00 },
+        { nome: "Papel A4", preco: 5.00 },
+        { nome: "Almofada", preco: 15.00 },
+        { nome: "Copo", preco: 3.50 },
+        { nome: "Toalha de mesa", preco: 8.00 },
+        { nome: "Computador", preco: 800.00 },
+        { nome: "Mouse", preco: 20.00 },
+        { nome: "Teclado", preco: 30.00 },
+        { nome: "Monitor", preco: 150.00 },
+        { nome: "Impressora", preco: 100.00 },
+        { nome: "Fones de ouvido", preco: 50.00 },
+        { nome: "Mochila", preco: 40.00 },
+        { nome: "Carregador de celular", preco: 15.00 },
+        { nome: "Livro", preco: 12.00 },
+        { nome: "Cadeira", preco: 70.00 },
+        { nome: "Mesa", preco: 90.00 },
+        { nome: "Sofá", preco: 300.00 },
+        { nome: "Tapete", preco: 25.00 },
+        { nome: "TV", preco: 400.00 },
+        { nome: "DVD Player", preco: 50.00 },
+        { nome: "Vaso de flores", preco: 10.00 },
+        { nome: "Abajur", preco: 20.00 },
+        { nome: "Quadro decorativo", preco: 35.00 },
+        { nome: "Panela", preco: 18.00 },
+        { nome: "Frigideira", preco: 15.00 },
+        { nome: "Faca de cozinha", preco: 8.00 },
+    ];
+    
+    
+
+    return (
         <>
-        <Nav/>
+            <Nav />
+            <TableContainer>
+                <Table>
+                    <TableCaption placement="top">Compras cadastradas</TableCaption>
+                    <Thead>
+                        <Th>Vendedor</Th>
+                        <Th>Cliente</Th>
+                        <Th>Valor da compra</Th>
+                        <Th>Data da Compra</Th>
+                    </Thead>
+                    <Tbody>
+                        {compras.map((compra, index) => (
+                            <Tr key={index} onClick={() => openModal(compra)} cursor="pointer">
+                                <Td>{compra.vendedor}</Td>
+                                <Td>{compra.cliente}</Td>
+                                <Td>{compra.valor}</Td>
+                                <Td>{compra.data}</Td>
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            <Modal isOpen={isOpen} onClose={closeModal}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Detalhes da Compra</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Flex alignItems="center">
+                            <Text fontWeight="bold" color="teal.500" mr="2">Vendedor:</Text>
+                            <Text>{selectedCompra && selectedCompra.vendedor}</Text>
+                        </Flex>
+                        <Flex alignItems="center">
+                            <Text fontWeight="bold" color="teal.500" mr="2">Cliente:</Text>
+                            <Text>{selectedCompra && selectedCompra.cliente}</Text>
+                        </Flex>
+                        <Flex alignItems="center">
+                            <Text fontWeight="bold" color="teal.500" mr="2">Valor da Compra:</Text>
+                            <Text>{selectedCompra && selectedCompra.valor}</Text>
+                        </Flex>
+                        <Flex alignItems="center">
+                            <Text fontWeight="bold" color="teal.500" mr="2">Data da Compra:</Text>
+                            <Text>{selectedCompra && selectedCompra.data}</Text>
+                        </Flex>
+
+                        <TableContainer maxHeight="300px" overflowY="auto">
+                            <Table>
+                                <TableCaption placement="top">Produtos da compra</TableCaption>
+                                <Thead>
+                                    <Th>Produto</Th>
+                                    <Th>Preço</Th>
+                                </Thead>
+                                <Tbody>
+                                {produtos.map((produto, index) => (
+                                    <Tr key={index}>
+                                        <Td>{produto.nome}</Td>
+                                        <Td>{produto.preco}</Td>
+                                    </Tr>
+                                ))}
+                                </Tbody>
+                            </Table>
+                        </TableContainer>
+
+                    </ModalBody>
+
+                    <ModalFooter>
+                        
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     )
 }
